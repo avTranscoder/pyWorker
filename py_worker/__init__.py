b204c183072294d72b8e8c75b280a4a824344f7d
Marc-Antoine Arnaud <arnaud@ebu.ch>
2016-11-22 14:59:21 +0100

    support python3, add small ui, and support analysis level

--- py_worker/__init__.py
+++ py_worker/__init__.py
@@ -5,4 +5,4 @@ __version__ = '0.1.0'
 
 app = Flask(__name__)
 
-import views
+from py_worker import views
--- py_worker/views.py
+++ py_worker/views.py
@@ -1,13 +1,16 @@
+import os
 import json
 
-from flask import request, Response, abort, jsonify
+from flask import request, Response, abort, jsonify, send_file
 
 from py_worker import app
 from pyAvTranscoder import avtranscoder as av
 
+# av.Logger().setLogLevel(av.AV_LOG_QUIET)
+
 @app.route('/ping')
 def api_ping():
-    return 'pong'
+    return jsonify({'ping': 'pong'})
 
 @app.route('/')
 def index():
@@ -35,9 +38,39 @@ def index():
     }
     return jsonify(**infos)
 
+
+@app.route('/ui')
+def small_ui():
+    return send_file(os.path.join("interface", "index.html"))
+
 @app.route('/probe', methods=['GET'])
 def get_probe_on_file():
     path = request.args.get('path')
+
+    if path == None:
+        abort(400)
+
+    level = request.args.get('level', 'fast')
+
+    analyse_gop_state = "disabled"
+    if level == 'firstGop':
+        analyse_gop_state = "enabled"
+
+    app.logger.info('Analyse path: %s - GOP analysis: %s', path, analyse_gop_state)
+    av.preloadCodecsAndFormats()
+    inputFile = av.InputFile(str(path))
+    if level == 'firstGop':
+        inputFile.analyse(av.NoDisplayProgress(), av.eAnalyseLevelFirstGop)
+    data = inputFile.getProperties().allPropertiesAsJson()
+
+    response = Response(data, mimetype='application/json')
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+
+@app.route('/thumbnail', methods=['GET'])
+def get_thumbnail_on_file():
+    path = request.args.get('path')
+    frame = float(request.args.get('frame', 0))
 
     if path == None:
         abort(400)
@@ -51,10 +84,60 @@ def get_probe_on_file():
     app.logger.info('Analyse path: %s - GOP analysis: %s', path, analyse_gop_state)
     av.preloadCodecsAndFormats()
     inputFile = av.InputFile(str(path))
-    # if analyse_gop:
-    #     inputFile.analyse(av.NoDisplayProgress(), av.eAnalyseLevelFirstGop)
-    data = inputFile.getProperties().allPropertiesAsJson()
-    return Response(data, mimetype='application/json')
+
+    # inputFile.seekAtFrame(frame, av.AVSEEK_FLAG_FRAME)
+    # inputFile.seekAtFrame(frame, av.AVSEEK_FLAG_ANY)
+    # inputFile.seekAtFrame(frame, av.AVSEEK_FLAG_BACKWARD)
+
+    tmpFilename = "/tmp/thumbnails.jpg"
+
+    # create output file (need to set format profile of encoding to force output format to mjpeg)
+    formatProfile = av.ProfileMap()
+    formatProfile[av.avProfileIdentificator] = "thumbnailFormatPreset"
+    formatProfile[av.avProfileIdentificatorHuman] = "Thumbnail format preset"
+    formatProfile[av.avProfileType] = av.avProfileTypeFormat
+    formatProfile[av.avProfileFormat] = "mjpeg"
+    outputFile = av.OutputFile(tmpFilename)
+    outputFile.setupWrapping(formatProfile)
+
+    # create input stream
+    videoProperties = inputFile.getProperties().getVideoProperties()[0]
+    videoStreamIndex = videoProperties.getStreamIndex()
+    videoWidth = videoProperties.getWidth()
+    videoHeight = videoProperties.getHeight()
+    videoFps = videoProperties.getFps()
+    dar = videoProperties.getDar()
+
+    # inputStream = av.InputStream(inputFile, videoStreamIndex)
+    # inputStream.activate()
+
+    inputStreamDesc = av.InputStreamDesc(str(path), videoStreamIndex)
+
+    # create output stream
+    videoProfile = av.ProfileMap()
+    videoProfile[av.avProfileIdentificator] = "thumbnailVideoPreset"
+    videoProfile[av.avProfileIdentificatorHuman] = "Thumbnail video preset"
+    videoProfile[av.avProfileType] = av.avProfileTypeVideo
+    videoProfile[av.avProfileCodec] = "mjpeg"
+    # videoProfile[av.avProfilePixelFormat] = "yuvj420p"
+    videoProfile[av.avProfilePixelFormat] = "yuvj422p"
+    if 'width' in request.args:
+        videoProfile[av.avProfileWidth] = str(request.args["width"])
+        videoProfile[av.avProfileHeight] = str(videoHeight*int(request.args["width"])/videoWidth)
+    # if 'height' in request.args:
+    #     videoProfile[av.avProfileHeight] = str(request.args["height"])
+
+    # create transcoder
+    transcoder = av.Transcoder(outputFile)
+    transcoder.addStream(inputStreamDesc, videoProfile, -frame/25)
+
+    # launch process
+    outputFile.beginWrap()
+    transcoder.preProcessCodecLatency()
+    transcoder.processFrame()
+    outputFile.endWrap()
+
+    return send_file(tmpFilename, mimetype='image/gif')
 
 @app.route('/jobs', methods=['GET'])
 def get_all_jobs():
